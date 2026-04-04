7b99c169323b6d91f867821fa1292975c9ada320
YAOmeihah <741937692@qq.com>
2026-04-04 10:19:59 +0800

    feat: configure vpay admin runtime

--- frontend/admin/vite.config.ts
+++ frontend/admin/vite.config.ts
@@ -1,6 +1,11 @@
 import { getPluginsList } from "./build/plugins";
 import { include, exclude } from "./build/optimize";
-import { type UserConfigExport, type ConfigEnv, loadEnv } from "vite";
+import {
+  type UserConfigExport,
+  type ConfigEnv,
+  type ProxyOptions,
+  loadEnv
+} from "vite";
 import {
   root,
   alias,
@@ -9,9 +14,28 @@ import {
   __APP_INFO__
 } from "./build/utils";
 
+const proxyPaths = ["/login", "/getMenu", "/admin", "/enQrcode", "/qr-code"];
+
 export default ({ mode }: ConfigEnv): UserConfigExport => {
-  const { VITE_CDN, VITE_PORT, VITE_COMPRESSION, VITE_PUBLIC_PATH } =
-    wrapperEnv(loadEnv(mode, root));
+  const {
+    VITE_CDN,
+    VITE_PORT,
+    VITE_COMPRESSION,
+    VITE_PUBLIC_PATH,
+    VITE_PROXY,
+    VITE_PROXY_DOMAIN
+  } = wrapperEnv(loadEnv(mode, root));
+
+  const proxy: Record<string, ProxyOptions> =
+    VITE_PROXY && VITE_PROXY_DOMAIN
+      ? proxyPaths.reduce((prev, path) => {
+          prev[path] = {
+            target: VITE_PROXY_DOMAIN,
+            changeOrigin: true
+          };
+          return prev;
+        }, {} as Record<string, ProxyOptions>)
+      : {};
   return {
     base: VITE_PUBLIC_PATH,
     root,
@@ -24,7 +48,7 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
       port: VITE_PORT,
       host: "0.0.0.0",
       // 本地跨域代理 https://cn.vitejs.dev/config/server-options.html#server-proxy
-      proxy: {},
+      proxy,
       // 预热文件以提前转换和缓存结果，降低启动期间的初始页面加载时长并防止转换瀑布
       warmup: {
         clientFiles: ["./index.html", "./src/{views,components}/*"]
@@ -39,6 +63,8 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
     build: {
       // https://cn.vitejs.dev/guide/build.html#browser-compatibility
       target: "es2015",
+      outDir: "../../public/console",
+      emptyOutDir: true,
       sourcemap: false,
       // 消除打包大小超过500kb警告
       chunkSizeWarningLimit: 4000,
